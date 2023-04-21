a3d80ed296a75f3e93bfd6c8e9467f3e826686a2
AIE\s210991 <s210991@students.aie.edu.au>
2023-04-21 11:02:12 +0930

    Changed text.

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -6,7 +6,7 @@ int main()
 	std::cout << "We hope your brief detention in the relaxation vault has been a pleasant one." << std::endl;
 	std::cout << "Your specimen has been processed and we are now ready to begin the test proper." << std::endl;
 	std::cout << "Before we start, however, keep in mind although fun and learning are the primary goals of enrichment center activities, serious injuries may occur." << std::endl;
-	std::cout << "For your own safety, and the safety of others, please refrain from " << std::endl;
+	std::cout << "For your own safety, and the safety of others, please refrain from exiting before saving..." << std::endl;
 
 	std::cin.ignore();
 	return 0;
